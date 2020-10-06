f65fad2d65dad7ef09fb38b6ac4d78f4a333bf50
simonyjs <cage@live.co.kr>
2020-10-06 14:27:42 +0900

    2.7 DOM - If else - Function practice Two

--- index.js
+++ index.js
@@ -1,59 +1,46 @@
-// Document Object Module DOM을 이용해서 HTML의 모든 구성요소를 Object로 바꿀 수 있음
-// 즉 JS를 통해 가져온 Object를 통해 모든 객체를 제어 가능하다
-const title = document.getElementById('title');
-console.log(title);
-title.innerHTML = 'Hi!!! This is inner HTML!';
-console.dir(title);
-title.style.color = 'red';
-// CSS와 동일하게 사용을 하려면 음... 비슷하게 적용하개 하려면
-// Queryselector를 사용한다
-// Returns the first element that is a descendant of node that matches selectors.
-// querySelector는 셀렉터와 일치하는 노드의 첫번째 자식을 반환하고 일치하는 요소가 없으면 null을 반환한다.
-const titleQuery = document.querySelector('#title');
-titleQuery.style.color = 'black';
-// addEventListener는 이벤트를 감지(Listen)한다
-// 윈도우에서 리사이즈 이벤트가 발생하면 함수로 정한 handleResize를 호출한다
-// 반드시 호출만 해야한다 왜? 이벤트가 발생했을때만 원하는 함수를 실행하기 위해서이다. ()를 같이 사용하면 즉시 실행된다.
-// ()는 실행하는 버튼이다
-window.addEventListener('resize', handleResize);
-function handleResize() {
-  console.log('I have been RESIZED!!!');
-}
+// 2.7 DOM - If else - Function practice Two
+/*
+JS에서 CSS가 하는 일을 겹쳐서 하기 싫어서
+css에서 h1의 색깔을 지정하고 html에서 클래스를 지정함
+*/
+const title = document.querySelector('#title');
 
-title.addEventListener('click', handleClick);
+const CLICKED_CLASS = 'clicked';
+// === 는 !== 반대이다
+// 만약 currentClass 가 "clicked" 가 아니라면 (!==)
+// title의 className을 "clicked" 라고 부여하고 그렇지 않으면 빈칸으로 둔다
 function handleClick() {
-  title.style.color = 'blue';
-}
-
-// const age = prompt('How old are you?');
-
-// console.log(age);
-
-// if (age >= 19 && age <= 21) {
-//   console.log("You can Drink But you shouldn't");
-// } else if (age > 21) {
-//   console.log('You can Drink');
-// } else {
-//   console.log('You are Too Young');
-// }
-
-// 2.6 DOM - If else - Function practice
-// https://flatuicolors.com/palette/defo 에서 원하는 색깔을 가져와서 상수로 지정한다
-const BASE_COLOR = 'rgb(46, 204, 113)'; //rgb로 하지 않으면 오류가 발생
-const OTHER_COLOR = '#7f8c8d';
-function handleClick() {
-  const currentColor = title.style.color;
-  console.log(currentColor);
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
+  const currentClass = title.className;
+  if (currentClass !== CLICKED_CLASS) {
+    title.className = CLICKED_CLASS;
   } else {
-    title.style.color = BASE_COLOR;
+    title.className = '';
   }
 }
-// init 함수를 지정하고 실행한다
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener('click', handleClick);
 }
 init();
-// https://developer.mozilla.org/ko/docs/Web/Events 이벤트 참조
+// 문제는 click event 가 발생하면 class="clicked" 로 바뀌면서 class="button"이 없어진다
+// 그래서 classList를 활용해서 추가 삭제만 가능하게 한다.
+// function handleClick() {
+//   const hasClass = title.classList.contains(CLICKED_CLASS);
+//   if (hasClass) {
+//     title.classList.remove(CLICKED_CLASS);
+//   } else {
+//     title.classList.add(CLICKED_CLASS);
+//   }
+// }
+// //가독성이 좋게 함수를 바꿀 수 있음
+// function handleClick() {
+//   const hasClass = title.classList.contains(CLICKED_CLASS);
+//   if (!hasClass) {
+//     title.classList.add(CLICKED_CLASS);
+//   } else {
+//     title.classList.remove(CLICKED_CLASS);
+//   }
+// }
+// toggle 이용하기
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
