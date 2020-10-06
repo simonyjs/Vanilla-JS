5b4c76a98e4fea88be0b7358a858f2257fcaf4ec
simonyjs <cage@live.co.kr>
2020-10-06 10:14:12 +0900

    2.4 Events and event handlers

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ title.style.color = 'red';
 // querySelector는 셀렉터와 일치하는 노드의 첫번째 자식을 반환하고 일치하는 요소가 없으면 null을 반환한다.
 const titleQuery = document.querySelector('#title');
 titleQuery.style.color = 'black';
+// addEventListener는 이벤트를 감지(Listen)한다
+// 윈도우에서 리사이즈 이벤트가 발생하면 함수로 정한 handleResize를 호출한다
+// 반드시 호출만 해야한다 왜? 이벤트가 발생했을때만 원하는 함수를 실행하기 위해서이다. ()를 같이 사용하면 즉시 실행된다.
+// ()는 실행하는 버튼이다
+window.addEventListener('resize', handleResize);
+function handleResize() {
+  console.log('I have been RESIZED!!!');
+}
+
+title.addEventListener('click', handleClick);
+function handleClick() {
+  title.style.color = 'blue';
+}
