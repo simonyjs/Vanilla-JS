8b969383aebdc51bc5babec2f60e5f8fc55b397a
simonyjs <cage@live.co.kr>
2020-10-06 09:13:19 +0900

    2.3 설명 추가

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ console.dir(title);
 title.style.color = 'red';
 // CSS와 동일하게 사용을 하려면 음... 비슷하게 적용하개 하려면
 // Queryselector를 사용한다
+// Returns the first element that is a descendant of node that matches selectors.
+// querySelector는 셀렉터와 일치하는 노드의 첫번째 자식을 반환하고 일치하는 요소가 없으면 null을 반환한다.
 const titleQuery = document.querySelector('#title');
 titleQuery.style.color = 'black';
