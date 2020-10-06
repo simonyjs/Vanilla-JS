9a7f2afd7c8c4531c2135ae2923dc82d89457022
simonyjs <cage@live.co.kr>
2020-10-06 15:14:45 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -6,10 +6,17 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  // 조건부 삼항 연산자(ternary operator)는 JavaScript에서 세 개의 피연산자를 취할 수 있는 유일한 연산자입니다.
+  // 보통 if 명령문의 단축 형태로 쓰입니다.
+  // condition ? exprIfTrue : exprIfFalse
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 function init() {
   getTime();
+  //setInterval(function , miliseconds)
+  setInterval(getTime, 1000);
 }
 
 init();
